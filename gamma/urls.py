f50ac5154158a977d0fcca117fe147a35df1b096
Cameron Smith <cjhsmith8@gmail.com>
2021-03-02 14:26:23 +0000

    Updated base.html. Added css for it

--- gamma/urls.py
+++ gamma/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name="index"),
     path('register/', views.register, name="gamma-register"),
     path('login/', auth_views.LoginView.as_view(template_name='gamma/login.html'), name="gamma-login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='gamma/logout.html'), name="gamma-logout"),
