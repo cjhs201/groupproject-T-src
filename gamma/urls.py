85edfa562b8d9c23dd8e5f26cc4ec5dc99194912
Yass <yassvanth_p@yahoo.com>
2021-03-10 21:29:59 +0000

    added profiel points

--- gamma/urls.py
+++ gamma/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name="post-create"),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name="post-update"),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name="post-delete"),
-    #path('post/<int:pk>/completed', PostCompletedView.as_view(), name="post-completed"),
+    path('post/<int:pk>/completed', PostCompletedView.as_view(), name="post-completed"),
     path('register/', views.register, name="gamma-register"),
     path('register/tc', views.tc, name="gamma-register-tc"),
     path('login/', auth_views.LoginView.as_view(template_name='gamma/login.html'), name="gamma-login"),
